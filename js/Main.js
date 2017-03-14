5fa33a9f35afc6f663df58c4e66b54aedf31b4fd
AnthonyFusco <anthony.fusco42@gmail.fr>
2017-03-14 17:02:21 +0100

    correction after merge

--- js/Main.js
+++ js/Main.js
@@ -12,8 +12,8 @@ let game;
 let username;
 
 function initSocket(username){
-    //var socket = io.connect('http://192.168.43.3:8082');
-    var socket = io.connect('http://127.0.0.1:8082');
+    var socket = io.connect('http://192.168.43.3:8082');
+    // var socket = io.connect('http://127.0.0.1:8082');
 
     socket.on('connect', function(){
         socket.emit('adduser', username);
